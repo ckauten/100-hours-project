a4ad4870538a36a354024b32f5438348d2dfa45c
Cameron Kauten <cameron.kauten@gmail.com>
2024-04-10 15:28:06 -0700

    auth starting to be added. got login page setup with redirect to home page. Signup not currently working, redirects to home page immediatley

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,10 @@
 module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
-    }
-}
+  //starts the home page get request
+  getIndex: (req, res) => {
+    res.render('index.ejs');
+  },
+  //renders home page once logged in
+  getHome: (req, res) => {
+    res.render('home.ejs');
+  },
+};
